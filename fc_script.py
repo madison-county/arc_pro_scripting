f4ea9d67b60b54ac26b25d4916e47e5de2abd2eb
boykjgis <boykjgis@gmail.com>
2022-11-17 16:37:44 -0700

    fc_script.py: Add user input and default value for file_path

--- fc_script.py
+++ fc_script.py
@@ -5,10 +5,10 @@ import ctypes
 
 # Create a new feature class
 arcpy.env.workspace = (r"C:/Users/jboyk/map_stuff/test_project_2")
-file_path = (r"C:/Users/jboyk/map_stuff/")
 message_box = ctypes.windll.user32.MessageBoxW
 
 feature_class_name = arcpy.GetParameterAsText(0)
+file_path = (arcpy.GetParameterAsText(1)) or (r"C:/Users/jboyk/map_stuff/")
 
 if len(feature_class_name) > 13:
     err = "Error: 13 character limit max for inputs"
