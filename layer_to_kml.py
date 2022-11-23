f74aa8c51ef2b6a7047583c24a7756ff625687bf
boykjgis <boykjgis@gmail.com>
2022-11-23 16:04:35 -0700

    layer_to_kml.py: Implement basic version of LayerToKML for default values

--- layer_to_kml.py
+++ layer_to_kml.py
@@ -6,16 +6,20 @@ import os
 arcpy.env.workspace = "C:/Users/jboyk/map_stuff/test_project_2"
 
 layer_path = (r"C:/Users/jboyk/code_stuff/python/arc_pro_scripting/Montana_PublicLands.gdb")
-layer_file = "Montana_PublicLands_Layer"
-KML_OUT_PATH = (r"C:/Users/jboyk/map_stuff/test_project_2")
+LAYER_FILE = "cemetery_layer"
+KML_OUT_PATH = (r"C:/Users/jboyk/map_stuff/test_project_2/")
 
-layer_input = arcpy.GetParameterAsText(0)
-layer_output_path = arcpy.GetParameterAsText(1)
+layer_input = arcpy.GetParameterAsText(0) or LAYER_FILE
+layer_output_path = arcpy.GetParameterAsText(1) or KML_OUT_PATH
 composite = arcpy.GetParameterAsText(2) or 'COMPOSITE'
-pixels = arcpy.GetParameterAsText(3) or 8192
+pixels = arcpy.GetParameterAsText(3) or 4096
 dpi = arcpy.GetParameterAsText(4) or 96
 clamped = arcpy.GetParameterAsText(5) or 'CLAMPED_TO_GROUND'
 
+# Export a .kml file with all default values for parameters
+arcpy.conversion.LayerToKML(layer_input+".lyrx", layer_output_path+layer_input+'_simple_.kmz')
+
+# Export a kml file with a set scale and specified values for each parameter
 for scale in range(10000, 30001, 10000):
-    arcpy.conversion.LayerToKML("Montana_PublicLands_Layer.lyrx", 'out_kml.kmz', scale, composite,
+    arcpy.conversion.LayerToKML(layer_input+".lyrx", layer_output_path+layer_input+'_detailed_.kmz', scale, composite,
                                 '', pixels, dpi, clamped)
