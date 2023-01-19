8ddf8a64bde449b4734b7680b379f559999d4076
boykjgis <boykjgis@gmail.com>
2023-01-19 08:22:26 -0700

    test_login.py: Add password load for .env file

--- test_login.py
+++ test_login.py
@@ -1,3 +1,7 @@
 from arcgis.gis import *
 from arcgis.gis import GIS
 from dotenv import load_dotenv
+
+# Load user ArcGIS password from the .env file
+load_dotenv()
+ARCGIS_PASSWORD = os.getenv("ARCGIS_PW")
