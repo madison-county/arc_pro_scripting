33a0bac66f1e1302ef697b60db05de423fed5111
boykjgis <boykjgis@gmail.com>
2023-02-01 09:32:55 -0700

    AGOLBackup_01-19-2023.py: Add TODO for copyLocation

--- AGOLBackup_01-19-2023.py
+++ AGOLBackup_01-19-2023.py
@@ -108,6 +108,7 @@ if WorkingLocation == "4": # Testing
     newTempLocation = r'T:/01-Working_Data/03-Work_In_Progress/03-Jacob/AGOL/temps_local/'
     # TODO TempName
     # TODO temporaryOutWorkspace
+    # TODO copyLocation
     #originalRoadStructureLocation = r'mnt/c/Users/jboyk/code_stuff/online_backups/Roads'
     #originalRouteLocation = r'mnt/c/Users/jboyk/code_stuff/online_backups/Routing'
 
