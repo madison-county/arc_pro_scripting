e3446fd4f8f2d4da8d1496f7f149e358aaf70ab5
boykjgis <boykjgis@gmail.com>
2023-02-01 11:16:38 -0700

    dev_AGOL.py: Add copyPasteoverwrite function

--- dev_AGOL.py
+++ dev_AGOL.py
@@ -190,6 +190,25 @@ def ask_user(prompt):    ## This is the user input function that only evaluates 
         print(error)
         return ask_user('You entered an invalid input. Please enter \"Yes\" or \"No\" (or \"Y\" or \"N\"):\t\t\t')
 
+def copyPasteOverwrite(structuresFolder, copyLocation, initialStartTime):
+    root_src_dir = copyLocation
+    root_dst_dir = structuresFolder
+    
+    for src_dir, dirs, files in os.walk(root_src_dir):
+        dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                # in case of the src and dst are the same file
+                if os.path.samefile(src_file, dst_file):
+                    continue
+                os.remove(dst_file)
+            shutil.move(src_file, dst_dir)
+    TimeCalculation(initialStartTime)
+
 if __name__ == "__main__":
     main()
 
