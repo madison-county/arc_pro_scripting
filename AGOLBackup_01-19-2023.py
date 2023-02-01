394abc75dd8c3fdd44bf41873e33e675a20ba736
boykjgis <boykjgis@gmail.com>
2023-02-01 10:09:34 -0700

    AGOLBackup_01-19-2023.py: Add copyLocation testing path

--- AGOLBackup_01-19-2023.py
+++ AGOLBackup_01-19-2023.py
@@ -108,7 +108,7 @@ if WorkingLocation == "4": # Testing
     newTempLocation = r'T:/01-Working_Data/03-Work_In_Progress/03-Jacob/AGOL/temps_local/'
     TempName = 'Temp'
     temporaryOutWorkspace = outWorkspace + '/Temps' + label
-    # TODO copyLocation
+    copyLocation = structuresFolder + os.path.sep + 'TransferFolder'
     originalRoadStructureLocation = r'T:/01-Working_Data/03-Work_In_Progress/03-Jacob/AGOL/routing/'
     originalRouteLocation = r'T:/01-Working_Data/03-Work_In_Progress/03-Jacob/AGOL/original_routing/'
     newRouteLocation = r'T:/01-Working_Data/03-Work_In_Progress/03-Jacob/AGOL/new_routes/'
