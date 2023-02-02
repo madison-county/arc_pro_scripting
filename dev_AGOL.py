1d889d1af012128b464ecb5762528efb1f97b91a
boykjgis <boykjgis@gmail.com>
2023-02-02 12:33:33 -0700

    dev_AGOL.py: Remove calls for roads and routes separate functions: Add in two additional calls to new function, roads_routes_transfer with respective args

--- dev_AGOL.py
+++ dev_AGOL.py
@@ -55,8 +55,10 @@ def main():
     featureTransfer(output_file_name, FGDB_name, structuresFolder, label, initialStartTime)
     copyPasteOverwrite(structuresFolder, copyLocation, initialStartTime)
     CopyStucturesTempsToBackupFolder_G(label, outWorkspace, TempsG, temporaryOutWorkspace, initialStartTime, structuresFolder)
-    roads_transfer()
-    route_transfer()
+    #roads_transfer()
+    #route_transfer()
+    roads_routes_transfer('Roads', 'Are you connected to the server? [Y or N]:\n')
+    roads_routes_transfer('Routes', 'Transfer routes to the G Drive? [Y or N]:\n')
 
 def TimeCalculation(initialStartTime):    ## This is the time tracking code function.
     runTime = datetime.now() - initialStartTime
