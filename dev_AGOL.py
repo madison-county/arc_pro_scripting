f701ebaf2cdeef81db5e57101ae174f477f934e2
boykjgis <boykjgis@gmail.com>
2023-02-02 10:25:38 -0700

    dev_AGOL.py: Add control flows for yes or no answers in route_transfer func

--- dev_AGOL.py
+++ dev_AGOL.py
@@ -295,6 +295,11 @@ def RoutesToG(originalRouteLocation, newRouteLocation, initialStartTime):
 def route_transfer():
     user_prompt = input('Transfer routes to the G Drive? [Y or N]:\n').upper()
     print(user_prompt)
+    if user_prompt == 'Y':
+        RoutesToG(originalRouteLocation, newRouteLocation, initialStartTime)
+        print('Routes have been transfered')
+    else:
+        print('Routes have not been transferred')
 
 if __name__ == "__main__":
     main()
