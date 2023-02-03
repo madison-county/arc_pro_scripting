93daf439a1e318af6d0253e8ed7e44217e56d795
boykjgis <boykjgis@gmail.com>
2023-02-02 17:27:35 -0700

    dev_AGOL.py: Replicate previous commit cchanges to Temp AGOL deletion

--- dev_AGOL.py
+++ dev_AGOL.py
@@ -101,7 +101,7 @@ def downloadFromAGOL(backupFolder, initialStartTime, ARCGIS_PASSWORD):    # Down
     
     # Delete the FGDB Items in AGOL-----------------------------------------------------------------------------------------
     coreFeature_item_fgdb.delete()
-    print('Temp file in AGOL deleted \t\t\t\t\t\t Line: {} \t'.format(get_line()) + str(time.ctime()))
+    print('Temp file in AGOL deleted \t\t\t\t\t\t Line: {} \t{}'.format(get_line(), time.ctime()))
     
     # Create Variables for output text file
     coreFeature_item_lyrs = coreFeature_item.layers
