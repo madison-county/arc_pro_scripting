c0d6879b7550fb0f7cfc07af227edaaaf79949b5
boykjgis <boykjgis@gmail.com>
2023-02-02 17:28:13 -0700

    dev_AGOL.py: Replicate previous commit cchanges to Backup summary

--- dev_AGOL.py
+++ dev_AGOL.py
@@ -123,7 +123,7 @@ def downloadFromAGOL(backupFolder, initialStartTime, ARCGIS_PASSWORD):    # Down
         outFile.write(""+"\n"+"\n")
     
     outFile.close()
-    print('Backup Summary Completed \t\t\t\t\t\t Line: {} \t'.format(get_line()) + str(time.ctime()))
+    print('Backup Summary Completed \t\t\t\t\t\t Line: {} \t{}'.format(get_line(), time.ctime()))
     FGDB_name = extractZipFile(input_file_name, output_file_name, initialStartTime)
     print('*** FGDB: {} ***'.format(FGDB_name))
     # TODO - Fix pathing issue here
